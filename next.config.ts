3325700b1c9dec600e24b9351e8d690da2552601
lhdlam <119838153+lhdlam@users.noreply.github.com>
2025-08-19 19:10:02 +0700

    same

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'tlttechnology.site'],
   },
   webpack(config) {
     config.module.rules.push({
